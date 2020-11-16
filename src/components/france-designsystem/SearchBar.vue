e93d6ab4e9e40b2103f37f63035521824f56f592
Ash_Crow <sylvain@ashtree.eu>
2020-11-16 15:49:56 +0100

    Create SearchBar component

---
+++ src/components/france-designsystem/SearchBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="rf-search-bar"
+    :class="{ 'rf-search-bar--lg': large }"
+    id="search-input"
+  >
+    <label class="rf-label" for="search-input-input">
+      {{ label }}
+    </label>
+    <input
+      class="rf-input"
+      :placeholder="placeholder"
+      type="search"
+      id="search-input-input"
+      name="search-input-input"
+    />
+    <button class="rf-btn" title="Rechercher">
+      <span> Rechercher </span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      default: "Rechercher",
+    },
+    large: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
