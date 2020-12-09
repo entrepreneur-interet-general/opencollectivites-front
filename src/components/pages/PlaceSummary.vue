4b9876a401149eec993c38f451204823cac1880f
Ash_Crow <sylvain@ashtree.eu>
2020-12-09 16:41:02 +0100

    Started working on the PlaceSummary

--- src/components/pages/PlaceSummary.vue
+++ src/components/pages/PlaceSummary.vue
@@ -1,15 +1,33 @@
 <template>
   <div id="opencollectivites">
     <div class="rf-container">
+      <BaseBreadcrumb :currentPage="currentPage" />
       <h1>Fiche commune: {{ name }}</h1>
     </div>
   </div>
 </template>
 
 <script>
+import BaseBreadcrumb from "../vue-gouvfr/BaseBreadcrumb.vue";
 export default {
+  components: { BaseBreadcrumb },
   name: "PlaceSummary",
   props: ["siren", "name"],
+  data() {
+    return {
+      collectivity: {},
+    };
+  },
+  computed: {
+    currentPage() {
+      return "Fiche commune: " + this.name;
+    },
+  },
+  mounted: {
+    getCollectivityData() {
+      this.collectivity = this.siren;
+    },
+  },
 };
 </script>
 
