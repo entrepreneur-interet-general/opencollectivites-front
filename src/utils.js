901d9095c6fb17a6303ffa25727daede84b288e5
Ash_Crow <sylvain@ashtree.eu>
2020-12-15 14:20:49 +0100

    [utils] moved the shortnamedCommunes array to the utils file

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,27 @@
 const formatNumber = number => {
     try {
-        return number.toLocaleString("fr-FR", { maximumFractionDigits: 2 });
+        return number.toLocaleString("fr-FR", { maximumFractionDigits: 1 });
     } catch (error) {
-        console.error(error);
+        // console.error(error);
         return "";
     }
 }
 
-export { formatNumber }
+const shortnamedCommunes = [
+    "by",
+    "bu",
+    "eu",
+    "gy",
+    "oz",
+    "oo",
+    "py",
+    "ri",
+    "ry",
+    "sy",
+    "ur",
+    "us",
+    "uz",
+    "y",
+];
+
+export { formatNumber, shortnamedCommunes }
