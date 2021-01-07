d49a76c22ddb8670b9dd0a6d1f10ea270ae95741
Ash_Crow <sylvain@ashtree.eu>
2021-01-07 11:58:51 +0100

    Removed call to error page that prevented even correct functionning

--- src/services/OpenCollectivitesDataService.js
+++ src/services/OpenCollectivitesDataService.js
@@ -1,21 +1,15 @@
-import apiCall from "../http-common";
-import Router from '../router'
+import apiCall from "../http-common"
 
 class OpenCollectivitesDataService {
-    listByName(name) {
-        return apiCall.get(`/france/all/${name}`);
-    }
-    communeBySiren(siren) {
-        return apiCall.get(`/france/communes/siren/${siren}/`).catch(
-            Router.push({
-                name: "error"
-            })
-
-        )
-    }
-    aspicFicheCommune(siren) {
-        return apiCall.get(`/aspic/fiche-commune/${siren}/`)
-    }
+  listByName(name) {
+    return apiCall.get(`/france/all/${name}`);
+  }
+  communeBySiren(siren) {
+    return apiCall.get(`/france/communes/siren/${siren}/`)
+  }
+  aspicFicheCommune(siren) {
+    return apiCall.get(`/aspic/fiche-commune/${siren}/`)
+  }
 }
 
 export default new OpenCollectivitesDataService();
